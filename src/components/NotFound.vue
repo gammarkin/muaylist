9672d5e8074681a0fd5af73cdefa0abeb0556e16
gammarkin <marcosrbnsf@gmail.com>
2024-08-02 09:10:27 -0300

    feat: adds not found route view

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,47 @@
+<script setup>
+	import {useRouter} from 'vue-router';
+
+	import Button from 'primevue/button';
+
+	const router = useRouter();
+
+	const goToHome = () => {
+		router.push('/');
+	};
+</script>
+
 <template>
-	<div class="card">
-		<p>Route Not Found</p>
+	<div class="container">
+		<p class="h1">404</p>
+
+		<p class="h2">Page not found</p>
+
+		<Button class="mt-5" severity="info" text="" @click="goToHome"
+			>Go back to Home</Button
+		>
 	</div>
 </template>
 
 <style scoped>
-	.read-the-docs {
-		color: #888;
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+
+		width: 100vw;
+		height: 100vh;
+	}
+	.h1 {
+		font-size: 15rem;
+		margin: 0;
+	}
+	.h2 {
+		font-size: 3rem;
+		margin: 0;
+		text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;
+	}
+	.mt-5 {
+		margin-top: 5rem;
 	}
 </style>

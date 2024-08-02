21ed8bc002084e5127ace430870e5fff6c625992
gammarkin <marcosrbnsf@gmail.com>
2024-08-02 09:10:39 -0300

    feat: adds static data to play with

---
+++ src/data/classes.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: 'Beginner',
+        desc: 'Learn the fundamentals of Muay Thai in a supportive environment.',
+        category: 'beginner',
+    },
+    {
+        name: 'Intermediate',
+        desc: 'Take your Muay Thai skills to the next lever with our intermediate classes.',
+        category: 'intermediate',
+    },
+    {
+        name: 'Advanced',
+        desc: 'Challenge yourself with our advanced Muay Thai classes and reach your full potential.',
+        category: 'advanced',
+    }
+]
--- src/data/moves.js
+++ src/data/moves.js
@@ -4,11 +4,41 @@ export default [
         src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-jab.webp',
         name: 'Jab',
         desc: 'Gancho es un golpe realizado horizontalmente con el puño que tiene como objetivo golpear al oponente en el costado de la cabeza.',
+        category: 'fist'
     },
     {
         id: 2,
         src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-direto.webp',
         name: 'Directo',
         desc: 'El Directo en Muay Thai es un golpe muy potente y efectivo, que se ejecuta con el puño trasero de la guardia, y debe lanzarse en línea recta hacia el oponente.',
+        category: 'fist'
     },
+    {
+        id: 3,
+        src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-teep.webp',
+        name: 'Patada lateral (Teep)',
+        desc: 'La patada Teep es una técnica importante del Muay Thai y se utiliza para mantener al oponente a una distancia segura o para desequilibrarlo.',
+        category: 'kick'
+    },
+    {
+        id: 4,
+        src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-joelhada.webp',
+        name: 'Rodilla',
+        desc: 'Existen diferentes tipos de codos en Muay Thai, pero te explicaré cómo se hace el codo circular u horizontal, que es el codo más común en Muay Thai.',
+        category: 'knee'
+    },
+    {
+        id: 5,
+        src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-cotovelada.webp',
+        name: 'Codo',
+        desc: 'Es importante recordar que dar codazos es una técnica peligrosa y sólo debe ser realizada por luchadores experimentados y en condiciones controladas. El uso inadecuado de los codos puede provocar lesiones graves, como cortes profundos y fracturas.',
+        category: 'elbow'
+    },
+    {
+        id: 6,
+        src: 'https://www.dicaseducacaofisica.info/wp-content/uploads/2023/10/muay-thai-clinch.webp',
+        name: 'Clinch',
+        desc: 'El clinch es una técnica muy importante en Muay Thai y se utiliza para controlar al oponente y asestar golpes.',
+        category: 'other'
+    }
 ]

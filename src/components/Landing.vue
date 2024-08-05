0201eb97333ec4a786c4f72fe4f2e57fca1d39bf
gammarkin <marcosrbnsf@gmail.com>
2024-08-05 09:27:14 -0300

    feat: adds filter and search for moves & changes css from landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -3,12 +3,17 @@
 	import Badge from 'primevue/badge';
 	import Button from 'primevue/button';
 	import Dialog from 'primevue/dialog';
+	import Toolbar from 'primevue/toolbar';
+	import InputText from 'primevue/inputtext';
+	import SelectButton from 'primevue/selectbutton';
 
 	import {ref} from 'vue';
 	import {useRouter} from 'vue-router';
 	import {useMoveStore} from '../stores/moveStore';
 
 	import classes from '../data/classes';
+
+	document.title = 'Muay thai academy';
 
 	const MAX_CHAR_LENGHT = 55;
 	const iconCategory = {
@@ -28,6 +33,11 @@
 	const moveStore = useMoveStore();
 	const router = useRouter();
 
+	const search = ref('');
+	const category = ref('');
+
+	const options = ref(['fist', 'kick', 'knee', 'elbow', 'other']);
+
 	const navigateToAdmin = () => {
 		numberOfTimesClicked.value += 1;
 
@@ -35,6 +45,8 @@
 			return router.push('/admin');
 		}
 	};
+
+	const getMoves = () => moveStore.moves;
 
 	const showProduct = (product) => {
 		moveStore.setSelectedMove(product);
@@ -108,10 +120,48 @@
 				instructors.
 			</p>
 
+			<Toolbar>
+				<template #center>
+					<i class="pi pi-search" />
+
+					<InputText
+						type="text"
+						v-model="search"
+						class="mx-1"
+						placeholder="Nombre del golpe"
+					/>
+				</template>
+
+				<template #end>
+					<SelectButton
+						:options="options"
+						v-model="category"
+						aria-labelledby="basic"
+					/>
+				</template>
+			</Toolbar>
+
 			<div class="cards">
 				<Card
 					@click="showProduct(move)"
-					v-for="move in moveStore.moves"
+					v-for="move in getMoves().filter((move) => {
+						if (!search && !category) {
+							return true;
+						}
+
+						if (search && !category) {
+							return move.name.toLowerCase().includes(search.toLowerCase());
+						}
+
+						if (category && !search) {
+							return move.category === category;
+						}
+
+						return (
+							move.category === category &&
+							move.name.toLowerCase().includes(search.toLowerCase())
+						);
+					})"
 					class="card"
 				>
 					<template #header>
@@ -161,7 +211,7 @@
 			</div>
 		</section>
 
-		<section id="classes" class="flex-column-center main">
+		<section id="classes" class="flex-column-center classes">
 			<Badge
 				style="margin: 1rem"
 				severity="secondary"
@@ -174,7 +224,7 @@
 				advanced techniques of Muay Thai.
 			</p>
 
-			<div class="cards">
+			<div class="cards-class">
 				<Card v-for="classItem in classes" class="card">
 					<template #header>
 						<img
@@ -212,7 +262,7 @@
 
 			<p class="h2 color-muted" style="width: 35%">
 				Have questions or want to learn more about our Muay Thai classes?
-				<a>Contact us</a> today!
+				<a class="mouse-over">Contact us</a> today!
 			</p>
 		</section>
 	</div>
@@ -250,7 +300,6 @@
 </template>
 
 <style scoped>
-	/* hide scrollbar but allow scrolling */
 	body {
 		-ms-overflow-style: none; /* for Internet Explorer, Edge */
 		scrollbar-width: none; /* for Firefox */
@@ -260,32 +309,39 @@
 	body::-webkit-scrollbar {
 		display: none; /* for Chrome, Safari, and Opera */
 	}
+
 	.header {
 		width: 100vw;
 		height: 4rem;
-
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
-
 		padding: 0 1rem;
 		box-sizing: border-box;
-
 		position: fixed;
 		z-index: 1;
-
 		background-color: #1c1b22;
 	}
+	.cards-class {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		width: 66%;
+	}
+
 	.flex {
 		display: flex;
 	}
+
 	.flex-column-center {
 		display: flex;
 		flex-direction: column;
-
 		align-items: center;
 		justify-content: center;
+		width: 99vw;
 	}
+
 	.h1 {
 		letter-spacing: -0.05em;
 		font-family: 'Manrope', sans-serif;
@@ -294,70 +350,73 @@
 		font-size: 3rem;
 		margin: 0;
 	}
+
 	.h2 {
 		font-family: 'Manrope', sans-serif;
 		font-size: 1.25rem;
 		line-height: 1.625;
 	}
-	.main {
-		height: 115vh;
 
+	.main {
 		display: flex;
+		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		height: auto;
+		min-height: 100vh;
+		padding-top: 6rem;
 	}
+
 	.cards {
 		display: flex;
-		align-items: center;
+		align-items: flex-start;
 		justify-content: center;
-
-		margin: 1.5rem;
-
 		flex-wrap: wrap;
 		width: 66%;
+		margin-top: 1.5rem;
+		flex-grow: 1;
 	}
+	.classes {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+	}
+
 	.card {
 		width: 20rem;
-
 		margin: 1.5rem;
 		padding: 1.2rem;
-
 		overflow: hidden;
 		cursor: pointer;
-
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		flex-direction: column;
-
 		text-align: center;
-
 		border: hsl(240, 7.1%, 22%) 1px solid;
-
 		font-family: 'Manrope', sans-serif;
 	}
+
 	.card:hover {
 		transform: scale(1.05);
 		transition: ease-out;
 	}
+
 	.image {
 		width: 4rem;
 		height: 4rem;
 		padding: 1rem;
-
 		border-radius: 9999px;
-
 		color: hsl(0, 0%, 0%);
 		background-color: hsl(0, 0%, 90.2%);
-
 		border-width: 0;
 		border-style: solid;
 		box-sizing: border-box;
 	}
+
 	.desc {
 		margin: 0;
 		padding: 0;
-
 		box-sizing: border-box;
 		border-width: 0;
 		border-style: solid;
@@ -366,14 +425,15 @@
 	.name {
 		margin: 0;
 		padding: 0;
-
 		font-weight: 700;
 		font-size: 1.25rem;
 		line-height: 1.75rem;
 	}
+
 	.color-muted {
 		color: hsl(60, 5.3%, 85.1%);
 	}
+
 	.content {
 		padding: 0;
 	}
@@ -381,57 +441,67 @@
 	.edit-image {
 		max-width: 35rem;
 	}
+
 	.d-flex-column {
 		display: flex;
 		flex-direction: column;
 	}
+
 	.mb-1 {
 		margin-bottom: 1rem;
 	}
+
 	.about {
 		display: flex;
 		align-items: center;
 		justify-content: space-evenly;
-
 		background-color: #3d3d43;
 		padding-top: 8rem;
 		padding-bottom: 8rem;
 	}
+
 	.about-texts {
 		font-family: 'Manrope', sans-serif;
 	}
+
 	.about-text {
 		font-size: 2.25rem;
 		line-height: 1.25;
-
 		letter-spacing: -0.05em;
 		font-weight: 700;
 		margin: 0;
 	}
+
 	.about-desc {
 		margin: 1rem 0rem;
 		line-height: 1.625;
-
 		font-size: 1.25rem;
 		max-width: 600px;
 	}
+
 	.about-buttons {
 		display: flex;
 		flex-direction: column;
-
 		width: 35vw;
 	}
+
 	.contact {
 		display: flex;
 		flex-direction: column;
-
 		background-color: #3d3d43;
 		padding: 8rem 10rem;
 	}
+
 	.button-classes {
 		margin-top: 4rem;
-
 		display: flex;
 		align-items: start;
+	}
+
+	.mx-1 {
+		margin: 0rem 1rem;
+	}
+	.mouse-over:hover {
+		cursor: pointer;
 	}
 </style>

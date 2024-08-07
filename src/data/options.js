ca5920e1d9f0f5c58bca437ac58315670410bfaf
gammarkin <marcosrbnsf@gmail.com>
2024-08-07 11:11:43 -0300

    feat: adds option to separated file

---
+++ src/data/options.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        value: 'all',
+        name: 'todos',
+    },
+    {
+        value: 'fist',
+        name: 'puño',
+    },
+    {
+        value: 'kick',
+        name: 'patada',
+    },
+    {
+        value: 'knee',
+        name: 'rodilla',
+    },
+    {
+        value: 'elbow',
+        name: 'codo',
+    },
+    {
+        value: 'other',
+        name: 'otros',
+    },
+]
